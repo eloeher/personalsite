d54cd557eb859b28d37af4616ade8ecf09f2a73e
loeher <loeher@umich.edu>
2020-12-13 14:53:46 -0600

    transition

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ document.body.onkeyup = function(event){
   if (event.keyCode == 68){
     // change class of body to darkmode
     document.body.classList.toggle("dark");
+    transition: all 300ms ease;
   }
 }
