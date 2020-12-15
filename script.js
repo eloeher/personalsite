c61bde4b6e6a940d800e062091ec262fa02e6535
loeher <loeher@umich.edu>
2020-12-15 11:00:44 -0600

    try now

--- script.js
+++ script.js
@@ -7,10 +7,3 @@ function switch_to_dark(event){
   document.body.classList.toggle("dark");
 }
 
-document.body.onkeyup = function(event){
-  console.log(event.keyCode);
-  if (event.keyCode == 68){
-    // change class of body to darkmode
-    document.body.classList.toggle("dark");
-  }
-}
